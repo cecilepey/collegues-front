81deea1688fadaf1dd135d62c70944072b4179bf
Cecile Peyras <peyrascecile@gmail.com>
2019-09-23 12:26:28 +0200

    ajout boutons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="btn-group btn-group-toggle">
-  <label class="btn-primary" ngbButtonLabel>
-    <input type="checkbox" ngbButton> NG bootstrap 
+  <label class="btn-success" ngbButtonLabel>
+    <input type= "checkbox" ngbButton> NG bootstrap 
   </label>
   </div>
-  <hr>
-  
+ 
     
 
   <button type="button" class="btn btn-secondary">bootstrap</button>
